07dbbe9f502738447cb067344b6bcdafcf8b600d
Andres Zapata <mosquera.wilson@correounivalle.edu.co>
2023-01-17 19:25:19 -0500

    Inicio el MiniProyecto 4 y creo la rama "Andrés"

--- src/miniproyecto4/MiniProyecto4.java
+++ src/miniproyecto4/MiniProyecto4.java
@@ -9,7 +9,7 @@ public class MiniProyecto4 {
 
     
     public static void main(String[] args) {
-        System.out.println("Aqui iniciamos el Proyecto 4");
+        System.out.println(" Aqui iniciamos el Proyecto 4 ");
     }
     
 }
