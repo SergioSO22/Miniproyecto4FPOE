b3e04cf99211d7f450f56c0aebf4ceb796352661
Andres Zapata <mosquera.wilson@correounivalle.edu.co>
2023-01-17 19:11:48 -0500

    Inicio el MiniProyecto 4

---
+++ src/miniproyecto4/MiniProyecto4.java
@@ -0,0 +1,15 @@
+
+package miniproyecto4;
+
+/**
+ *
+ * @author 
+ */
+public class MiniProyecto4 {
+
+    
+    public static void main(String[] args) {
+        System.out.println("Aqui iniciamos el Proyecto 4");
+    }
+    
+}
